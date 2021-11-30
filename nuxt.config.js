3a381f0411a0703a37e9c29d3538ce5e642017ba
Ialgen Allal <ialgen.allal@gmail.com>
2021-11-30 08:38:16 +0100

    emoji

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,6 +26,9 @@ export default { /*
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
   ],*/
+  meta: [
+    { charset: 'utf-8' },
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
